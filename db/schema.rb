60b9971fe01f6fa78dc424c6edc7fc7dd8e96633
Alexandra <arbeuerman@gmail.com>
2021-06-01 11:06:07 -0500

    adding database tables and data

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_06_01_153122) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.string "description"
+    t.string "duration"
+    t.string "category"
+    t.boolean "has_volunteer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "volunteers", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_volunteers_on_task_id"
+    t.index ["user_id"], name: "index_volunteers_on_user_id"
+  end
+
+  add_foreign_key "tasks", "users"
+  add_foreign_key "volunteers", "tasks"
+  add_foreign_key "volunteers", "users"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Volunteer.destroy_all
+Volunteer.reset_pk_sequence
+
+Task.destroy_all
+Task.reset_pk_sequence
+
+User.destroy_all
+User.reset_pk_sequence
+
+10.times do 
+    User.create(username: Faker::Movies::StarWars.character, 
+    password: "1234abcd", 
+    address: Faker::Address.street_address)
+end
+
+10.times do 
+    Task.create(name: Faker::Job.key_skill, 
+    description: Faker::Job.field, 
+    duration: ['1 hour', '4 hours', '10 minutes', '15 minutes'].sample,
+    category: Faker::Job.field,
+    has_volunteer: false,
+    user_id: User.all.sample.id)
+end
+
+3.times do 
+    Volunteer.create(user_id: User.all.sample.id, task_id: Task.all.sample.id)
+end
